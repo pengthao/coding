b66d4b3e10d62f33c1ad78c92ad3ec94d6d204d9
Peng <pengsmemberships@gmail.com>
2023-09-22 22:37:28 -0500

    challenges completed

--- challenges/challenge-has-unique-char/hasUniqueChars.js
+++ challenges/challenge-has-unique-char/hasUniqueChars.js
@@ -1,1 +1,39 @@
 // Write your code below
+
+//Given a word, return true if that word contains only unique characters. Return false otherwise.
+
+// For example:
+
+// hasUniqueChars("Monday")
+// // returns true
+
+// hasUniqueChars("Moonday")
+// // returns false
+
+// Uppercase and lowercase letters should be considered separately:
+
+// hasUniqueChars("Bob")
+// // returns true
+
+
+const uniqueWord = (word) => {
+    let wordArr = word.split('')
+    let container = []
+    for (let i = 0; i < wordArr.length; i++) {
+        for (let x = i+1; x < wordArr.length; x++) {
+            if(wordArr[i] === wordArr[x]) {
+                container.push(true)
+            } else {
+                container.push(false)
+            }
+        }
+    }
+    if(container.includes(true)) {
+        console.log(`The word ${word} does not contain unique values.`)
+    } else {
+        console.log(word);
+    }
+    return word
+}
+
+uniqueWord('monday')
--- challenges/challenge-is-palindrome/isPalindrome.js
+++ challenges/challenge-is-palindrome/isPalindrome.js
@@ -1,1 +1,40 @@
-// Write your code below
+// // Write your code below
+
+// Return true if this word is a palindrome. false if it is not. A palindrome is a word that is spelled the same backwards and forwards.
+
+// For example:
+
+// isPalindrom("a")
+// // returns true
+
+// isPalindrom("noon")
+// // returns true
+
+// isPalindrom("hello")
+// // returns false
+
+// Treat spaces and uppercase letters normally—so “Racecar” wouldn’t be a palindrome since “R” and “r” aren’t the same letter:
+
+// isPalindrom("Racecar")
+// // returns false
+
+// isPalindrom("racecar")
+// // returns true
+
+const wordCheckRev = (word) => {
+    wordSpl = word.split('');
+    backwards = []
+    for(let i = wordSpl.length-1; i >= 0; i--) {
+        backwards.push(wordSpl[i]);
+    }
+    const wordSplStr = wordSpl.join('')
+    const backwardsStr = backwards.join('')
+    if(wordSplStr === backwardsStr) {
+        console.log(word)
+    } else{
+        console.log(`${word} is not a palindrome.`)
+    }
+}
+
+wordCheckRev('hello')
+wordCheckRev('racecar')
